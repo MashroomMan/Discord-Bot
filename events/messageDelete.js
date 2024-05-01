463ca5db56bbbdf726ca88ed6e7e64d3a40657a5
MashroomMan <thecheesemaker@protonmail.ch>
2024-05-01 14:51:53 -0400

    Fixed filename typo

---
+++ events/messageDelete.js
@@ -0,0 +1,32 @@
+const { AuditLogEvent } = require('discord.js');
+
+module.exports = {
+  name: 'messageDelete',
+  async execute(message) {
+    let user;
+    let target;
+
+    const logs = message.guild.channels.cache.find(chan => chan.name === 'logs');
+    const entry = await message.guild.fetchAuditLogs({ type: AuditLogEvent.MessageDelete }).then(audit => audit.entries.first());
+
+    // console.log(entry.extra);
+    // console.log('\n \n \n \n \n \n');
+    // console.log(await message.guild.fetchAuditLogs({ type: AuditLogEvent.MessageDelete }).then(audit => audit.entries.first()));
+    // we defined entry above, so we can use it here to check the channel id
+    if (entry.extra.channel.id === message.channel.id
+    // then we are checking if the target is the same as the author id
+    && (entry.target.id === message.author.id)
+    // we are comparing time as audit logs are sometimes slow.
+    && (entry.createdTimestamp > (Date.now() - 5000)
+    // we want to check the count as audit logs stores the amount deleted in a channel
+    && entry.extra.count >= 1)) {
+      user = entry.executor.username;
+      target = entry.target.username;
+    } else {
+      // When all else fails, we can assume that the author has deleted their message.
+      user = message.author.username;
+      target = user;
+    }
+    logs.send(`${user} deleted "${message}" by ${target} in #${message.channel.name}`);
+  },
+};
